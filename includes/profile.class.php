5c02dde0ee70f4f6863df2eca9f689aded37313d
Nasser Rafie <contact@geminorum.ir>
2015-09-20 18:21:27 +0430

    semantic on last active & styles

--- includes/profile.class.php
+++ includes/profile.class.php
@@ -18,6 +18,7 @@ class gMemberProfile extends gPluginModuleCore
 			// if ( $gMemberNetwork->settings->get( 'search_authors', FALSE ) )
 			// 	add_filter( 'posts_search', array( &$this, 'posts_search' ) );
 		}
+
 	}
 
 	public function admin_init()
@@ -30,42 +31,60 @@ class gMemberProfile extends gPluginModuleCore
 		add_action( 'edit_user_profile_update', array( $this, 'edit_user_profile_update' ), 10, 1 );
 
 		if ( is_network_admin() ) {
+			add_action( 'admin_print_styles', array( &$this, 'admin_print_styles' ) );
 			add_filter( 'wpmu_users_columns', array( &$this, 'wpmu_users_columns' ) );
 			add_filter( 'manage_users_custom_column', array( &$this, 'manage_users_custom_column' ), 10, 3 );
 		}
 	}
 
-	public function manage_users_custom_column( $empty, $column_name, $user_id )
+	public function admin_print_styles()
 	{
-		if ( 'timestamps' != $column_name )
-			return $empty;
-
-		global $mode;
-
-		$html        = '';
-		$user        = get_user_by( 'id', $user_id );
-		$lastlogin   = get_user_meta( $user_id, 'lastlogin', TRUE );
-		$register_ip = get_user_meta( $user_id, 'register_ip', TRUE );
-		$lastactive  = function_exists( 'bp_get_last_activity' ) ? bp_get_last_activity( $user_id ) : FALSE;
-
-		$html .= '<table w1idth="100%;"></tbody>';
-		// $html .= mysql2date( ( 'list' == $mode ? 'Y/m/d' : 'Y/m/d \<\b\r \/\> g:i:s a' ), $user->user_registered );
-		$html .= '<tr><td>'.__( 'Registered', GMEMBER_TEXTDOMAIN ).'</td><td><code title="'.mysql2date( 'g:i:s a', $user->user_registered ).'">'.mysql2date( 'Y/m/d', $user->user_registered ).'</code></td></tr>';
-		$html .= '<tr><td>'.__( 'Register IP', GMEMBER_TEXTDOMAIN ).'</td><td><code>'.( $register_ip ? $register_ip : __( 'N/A', GMEMBER_TEXTDOMAIN ) ).'</code></td></tr>';
-		$html .= '<tr><td>'.__( 'Last Login', GMEMBER_TEXTDOMAIN ).'</td><td>'.( $lastlogin ? '<code title="'.mysql2date( 'g:i:s a', $lastlogin ).'">'.mysql2date( 'Y/m/d', $lastlogin ) : __( 'Never', GMEMBER_TEXTDOMAIN ) ).'</code></td></tr>';
-
-		if ( $lastactive ) // https://wordpress.org/plugins/buddypress-last-active-users-wp-admin/
-			$html .= '<tr><td>'.__( 'Last Active', GMEMBER_TEXTDOMAIN ).'</td><td>'.( $lastactive ? $lastactive : __( 'Never', GMEMBER_TEXTDOMAIN ) ).'</code></td></tr>';
-
-		$html .= '</tbody></table>';
-		return $html;
+		if ( 'users-network' == get_current_screen()->base )
+			gPluginFormHelper::linkStyleSheet( $this->constants['plugin_url'].'assets/css/network.admin.users.css', GMEMBER_VERSION );
 	}
 
+	// defaults: 'cb', 'username', 'name', 'email', 'registered', 'blogs'
 	public function wpmu_users_columns( $users_columns )
 	{
 		unset( $users_columns['registered'] );
 		$users_columns['timestamps'] = __( 'Timestamps', GMEMBER_TEXTDOMAIN );
 		return $users_columns;
+	}
+
+	public function manage_users_custom_column( $empty, $column_name, $user_id )
+	{
+
+		// echo 'CDSSS';
+		// gnetwork_dump($empty);
+
+		if ( 'timestamps' != $column_name )
+			return $empty;
+
+		$html        = '';
+		$user        = get_user_by( 'id', $user_id );
+		$lastlogin   = get_user_meta( $user_id, 'lastlogin', TRUE );
+		$register_ip = get_user_meta( $user_id, 'register_ip', TRUE );
+
+		$html .= '<table></tbody>';
+
+		$html .= '<tr><td>'.__( 'Registered', GMEMBER_TEXTDOMAIN ).'</td><td><code title="'
+			.mysql2date( 'g:i:s a', $user->user_registered ).'">'
+			.mysql2date( 'Y/m/d', $user->user_registered ).'</code></td></tr>';
+
+		$html .= '<tr><td>'.__( 'Register IP', GMEMBER_TEXTDOMAIN ).'</td><td><code>'
+			.( $register_ip ? $register_ip : __( 'N/A', GMEMBER_TEXTDOMAIN ) ).'</code></td></tr>';
+
+		$html .= '<tr><td>'.__( 'Last Login', GMEMBER_TEXTDOMAIN ).'</td><td>'
+			.( $lastlogin ? '<code title="'.mysql2date( 'g:i:s a', $lastlogin ).'">'
+				.mysql2date( 'Y/m/d', $lastlogin ).'</code>'
+			: __( 'Never', GMEMBER_TEXTDOMAIN ) ).'</td></tr>';
+
+		if ( function_exists( 'bp_get_last_activity' ) )
+			$html .= '<tr><td colspan="2">'.bp_get_last_activity( $user_id ).'</td></tr>';
+
+		$html .= '</tbody></table>';
+
+		echo $html;
 	}
 
 	public function set_current_user()
