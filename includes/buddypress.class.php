7313bb8feca4baae0d8e24c01c14be68b7310334
Nasser Rafie <contact@geminorum.ir>
2015-09-22 00:26:10 +0330

    :wrench:

--- includes/buddypress.class.php
+++ includes/buddypress.class.php
@@ -2,11 +2,11 @@
 
 class gMemberBuddyPress extends gPluginModuleCore
 {
-	
+
 	public function setup_actions()
 	{
 		add_filter( 'bp_displayed_user_fullname', array( &$this, 'bp_displayed_user_fullname' ), 9 );
-		add_filter( 'bp_get_member_name' , array( &$this, 'bp_get_member_name' ), 9 );
+		add_filter( 'bp_get_member_name', array( &$this, 'bp_get_member_name' ), 9 );
 	}
 
 	public function bp_displayed_user_fullname( $default )
@@ -38,37 +38,37 @@ class gMemberBuddyPress extends gPluginModuleCore
 		$display_name = NULL;
 
 		switch( $gMemberNetwork->settings->get( 'bp_display_name', 'nickname' ) ) {
-			
+
 			case 'default' :
-			
+
 				$display_name = $default;
-				
+
 			break;
 			case 'first_last_name' :
-			
+
 				$display_name = sprintf( ' %s %s',
 					get_user_meta( $userdata->id, 'first_name', TRUE ),
 					get_user_meta( $userdata->id, 'last_name' , TRUE ) );
-					
+
 			break;
 			case 'username' :
-			
+
 				$display_name = $user->user_login;
-				
+
 			break;
 			case 'first_name' :
-			
+
 				$display_name = get_user_meta( $userdata->id, 'first_name', TRUE );
-				
+
 			break;
 			case 'last_name' :
-			
+
 				$display_name = get_user_meta( $userdata->id, 'last_name', TRUE );
 
 			break;
 			default :
 			case 'nickname' :
-			
+
 				$display_name = get_user_meta( $userdata->id, 'nickname', TRUE );
 		}
 
