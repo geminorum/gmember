e8564f76895556b8209ba948891f28736dde6199
Nasser Rafie <contact@geminorum.ir>
2015-09-21 21:39:47 +0430

    timestamps moved to admin module

--- includes/admin.class.php
+++ includes/admin.class.php
@@ -5,7 +5,18 @@ class gMemberAdmin extends gPluginModuleCore
 
 	public function setup_actions()
 	{
-		add_action( 'wp_network_dashboard_setup', array( &$this, 'wp_network_dashboard_setup' ) );
+		if ( is_network_admin() ) {
+			add_action( 'admin_print_styles', array( &$this, 'admin_print_styles' ) );
+			add_action( 'wp_network_dashboard_setup', array( &$this, 'wp_network_dashboard_setup' ) );
+			add_filter( 'wpmu_users_columns', array( &$this, 'wpmu_users_columns' ) );
+			add_filter( 'manage_users_custom_column', array( &$this, 'manage_users_custom_column' ), 10, 3 );
+		}
+	}
+
+	public function admin_print_styles()
+	{
+		if ( 'users-network' == get_current_screen()->base )
+			gPluginFormHelper::linkStyleSheet( $this->constants['plugin_url'].'assets/css/network.admin.users.css', GMEMBER_VERSION );
 	}
 
 	public function wp_network_dashboard_setup()
@@ -74,5 +85,45 @@ class gMemberAdmin extends gPluginModuleCore
 				echo '<li>'.sprintf( __( 'Total Users: %s', GMEMBER_TEXTDOMAIN ), number_format_i18n( $query->get_total() ) ).'</li>';
 			echo '</ul>';
 		}
+	}
+
+	// defaults: 'cb', 'username', 'name', 'email', 'registered', 'blogs'
+	public function wpmu_users_columns( $users_columns )
+	{
+		unset( $users_columns['registered'] );
+		$users_columns['timestamps'] = __( 'Timestamps', GMEMBER_TEXTDOMAIN );
+		return $users_columns;
+	}
+
+	public function manage_users_custom_column( $empty, $column_name, $user_id )
+	{
+		if ( 'timestamps' != $column_name )
+			return $empty;
+
+		$html        = '';
+		$user        = get_user_by( 'id', $user_id );
+		$lastlogin   = get_user_meta( $user_id, 'lastlogin', TRUE );
+		$register_ip = get_user_meta( $user_id, 'register_ip', TRUE );
+
+		$html .= '<table></tbody>';
+
+		$html .= '<tr><td>'.__( 'Registered', GMEMBER_TEXTDOMAIN ).'</td><td><code title="'
+			.mysql2date( 'g:i:s a', $user->user_registered ).'">'
+			.mysql2date( 'Y/m/d', $user->user_registered ).'</code></td></tr>';
+
+		$html .= '<tr><td>'.__( 'Register IP', GMEMBER_TEXTDOMAIN ).'</td><td><code>'
+			.( $register_ip ? $register_ip : __( 'N/A', GMEMBER_TEXTDOMAIN ) ).'</code></td></tr>';
+
+		$html .= '<tr><td>'.__( 'Last Login', GMEMBER_TEXTDOMAIN ).'</td><td>'
+			.( $lastlogin ? '<code title="'.mysql2date( 'g:i:s a', $lastlogin ).'">'
+				.mysql2date( 'Y/m/d', $lastlogin ).'</code>'
+			: __( 'Never', GMEMBER_TEXTDOMAIN ) ).'</td></tr>';
+
+		if ( function_exists( 'bp_get_last_activity' ) )
+			$html .= '<tr><td colspan="2">'.bp_get_last_activity( $user_id ).'</td></tr>';
+
+		$html .= '</tbody></table>';
+
+		echo $html;
 	}
 }
--- includes/profile.class.php
+++ includes/profile.class.php
@@ -29,62 +29,6 @@ class gMemberProfile extends gPluginModuleCore
 		add_action( 'personal_options', array( $this, 'personal_options' ), 12, 1 );
 		add_action( 'personal_options_update', array( $this, 'edit_user_profile_update' ), 10, 1 );
 		add_action( 'edit_user_profile_update', array( $this, 'edit_user_profile_update' ), 10, 1 );
-
-		if ( is_network_admin() ) {
-			add_action( 'admin_print_styles', array( &$this, 'admin_print_styles' ) );
-			add_filter( 'wpmu_users_columns', array( &$this, 'wpmu_users_columns' ) );
-			add_filter( 'manage_users_custom_column', array( &$this, 'manage_users_custom_column' ), 10, 3 );
-		}
-	}
-
-	public function admin_print_styles()
-	{
-		if ( 'users-network' == get_current_screen()->base )
-			gPluginFormHelper::linkStyleSheet( $this->constants['plugin_url'].'assets/css/network.admin.users.css', GMEMBER_VERSION );
-	}
-
-	// defaults: 'cb', 'username', 'name', 'email', 'registered', 'blogs'
-	public function wpmu_users_columns( $users_columns )
-	{
-		unset( $users_columns['registered'] );
-		$users_columns['timestamps'] = __( 'Timestamps', GMEMBER_TEXTDOMAIN );
-		return $users_columns;
-	}
-
-	public function manage_users_custom_column( $empty, $column_name, $user_id )
-	{
-
-		// echo 'CDSSS';
-		// gnetwork_dump($empty);
-
-		if ( 'timestamps' != $column_name )
-			return $empty;
-
-		$html        = '';
-		$user        = get_user_by( 'id', $user_id );
-		$lastlogin   = get_user_meta( $user_id, 'lastlogin', TRUE );
-		$register_ip = get_user_meta( $user_id, 'register_ip', TRUE );
-
-		$html .= '<table></tbody>';
-
-		$html .= '<tr><td>'.__( 'Registered', GMEMBER_TEXTDOMAIN ).'</td><td><code title="'
-			.mysql2date( 'g:i:s a', $user->user_registered ).'">'
-			.mysql2date( 'Y/m/d', $user->user_registered ).'</code></td></tr>';
-
-		$html .= '<tr><td>'.__( 'Register IP', GMEMBER_TEXTDOMAIN ).'</td><td><code>'
-			.( $register_ip ? $register_ip : __( 'N/A', GMEMBER_TEXTDOMAIN ) ).'</code></td></tr>';
-
-		$html .= '<tr><td>'.__( 'Last Login', GMEMBER_TEXTDOMAIN ).'</td><td>'
-			.( $lastlogin ? '<code title="'.mysql2date( 'g:i:s a', $lastlogin ).'">'
-				.mysql2date( 'Y/m/d', $lastlogin ).'</code>'
-			: __( 'Never', GMEMBER_TEXTDOMAIN ) ).'</td></tr>';
-
-		if ( function_exists( 'bp_get_last_activity' ) )
-			$html .= '<tr><td colspan="2">'.bp_get_last_activity( $user_id ).'</td></tr>';
-
-		$html .= '</tbody></table>';
-
-		echo $html;
 	}
 
 	public function set_current_user()
